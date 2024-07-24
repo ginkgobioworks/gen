ac1f4fce5f32682873979ad5061707ebb1ce67de
hofer <hofer@ginkgobioworks.com>
2024-07-24 19:51:40 -0400

    GFA import

--- src/main.rs
+++ src/main.rs
@@ -65,6 +65,67 @@ enum Commands {
     },
 }
 
+fn import_gfa(gfa_path: &String, collection_name: &String, conn: &mut Connection) {
+    run_migrations(conn);
+
+    let parser = GFAParser::new();
+    let gfa: GFA<Vec<u8>, ()> = parser.parse_file(gfa_path).unwrap();
+
+    let collection = models::Collection::create(conn, collection_name);
+
+    let mut blocks_by_segment_name: HashMap<String, Block> = HashMap::new();
+
+    for segment in &gfa.segments {
+        let sequence = String::from_utf8(segment.sequence.clone()).unwrap();
+        let seq_hash = models::Sequence::create(conn, "DNA".to_string(), &sequence, true);
+        let block = Block {
+            id: 0,
+            path_id: 0,
+            sequence_hash: seq_hash,
+            start: 0,
+            end: (sequence.len() as i32),
+            strand: "1".to_string(),
+        };
+        let segment_name = String::from_utf8(segment.name.clone()).unwrap();
+        blocks_by_segment_name.insert(segment_name, block);
+    }
+
+    let mut created_blocks_by_segment_name: HashMap<String, Block> = HashMap::new();
+
+    for input_path in &gfa.paths {
+        let path_name = String::from_utf8(input_path.path_name.clone()).unwrap();
+        let path = models::Path::create(conn, &collection.name, None, &path_name, Some(1));
+        for (name, _) in input_path.iter() {
+            let i = 0;
+            let block = blocks_by_segment_name.get(&name.to_string()).unwrap();
+            let created_block = Block::create(
+                conn,
+                &block.sequence_hash,
+                path.id,
+                block.start,
+                block.end,
+                &block.strand,
+            );
+            created_blocks_by_segment_name.insert(name.to_string(), created_block);
+        }
+    }
+
+    for link in &gfa.links {
+        let source_name = String::from_utf8(link.from_segment.clone()).unwrap();
+        let target_name = String::from_utf8(link.to_segment.clone()).unwrap();
+        let source_block = created_blocks_by_segment_name.get(&source_name).unwrap();
+        let target_block = created_blocks_by_segment_name.get(&target_name).unwrap();
+        models::Edge::create(conn, source_block.id, Some(target_block.id));
+    }
+
+    if !gfa.links.is_empty() {
+        let last_link = gfa.links.last().unwrap();
+        let source_name = String::from_utf8(last_link.to_segment.clone()).unwrap();
+        let source_block = created_blocks_by_segment_name.get(&source_name).unwrap();
+        models::Edge::create(conn, source_block.id, None);
+    }
+}
+
 fn main() {
     let cli = Cli::parse();
 
@@ -107,5 +168,131 @@ fn main() {
             _ => {}
         },
         None => {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rusqlite::Connection;
+    use std::fs;
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+    use gen::test_helpers::get_connection;
+
+    #[test]
+    fn test_add_fasta() {
+        let mut fasta_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        fasta_path.push("fixtures/simple.fa");
+        let mut conn = get_connection(None);
+        import_fasta(
+            &fasta_path.to_str().unwrap().to_string(),
+            "test",
+            false,
+            &mut conn,
+        );
+        assert_eq!(
+            BlockGroup::get_all_sequences(&conn, 1),
+            HashSet::from_iter(vec!["ATCGATCGATCGATCGATCGGGAACACACAGAGA".to_string()])
+        );
+
+        let path = Path::get(&conn, 1);
+        assert_eq!(
+            Path::sequence(&conn, path),
+            "ATCGATCGATCGATCGATCGGGAACACACAGAGA".to_string()
+        );
+    }
+
+    #[test]
+    fn test_update_fasta_with_vcf() {
+        let mut vcf_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        vcf_path.push("fixtures/simple.vcf");
+        let mut fasta_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        fasta_path.push("fixtures/simple.fa");
+        let conn = &mut get_connection("test.db");
+        let collection = "test".to_string();
+        import_fasta(
+            &fasta_path.to_str().unwrap().to_string(),
+            &collection,
+            false,
+            conn,
+        );
+        update_with_vcf(
+            &vcf_path.to_str().unwrap().to_string(),
+            &collection,
+            "".to_string(),
+            "".to_string(),
+            conn,
+        );
+        assert_eq!(
+            BlockGroup::get_all_sequences(conn, 1),
+            HashSet::from_iter(vec!["ATCGATCGATCGATCGATCGGGAACACACAGAGA".to_string()])
+        );
+        // A homozygous set of variants should only return 1 sequence
+        assert_eq!(
+            BlockGroup::get_all_sequences(conn, 2),
+            HashSet::from_iter(vec!["ATCATCGATAGAGATCGATCGGGAACACACAGAGA".to_string()])
+        );
+        // This individual is homozygous for the first variant and does not contain the second
+        assert_eq!(
+            BlockGroup::get_all_sequences(conn, 3),
+            HashSet::from_iter(vec!["ATCATCGATCGATCGATCGGGAACACACAGAGA".to_string()])
+        );
+    }
+
+    #[test]
+    fn test_update_fasta_with_vcf_custom_genotype() {
+        let mut vcf_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        vcf_path.push("fixtures/general.vcf");
+        let mut fasta_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        fasta_path.push("fixtures/simple.fa");
+        let conn = &mut get_connection("test.db");
+        let collection = "test".to_string();
+        import_fasta(
+            &fasta_path.to_str().unwrap().to_string(),
+            &collection,
+            false,
+            conn,
+        );
+        update_with_vcf(
+            &vcf_path.to_str().unwrap().to_string(),
+            &collection,
+            "0/1".to_string(),
+            "sample 1".to_string(),
+            conn,
+        );
+        assert_eq!(
+            BlockGroup::get_all_sequences(conn, 1),
+            HashSet::from_iter(vec!["ATCGATCGATCGATCGATCGGGAACACACAGAGA".to_string()])
+        );
+        assert_eq!(
+            BlockGroup::get_all_sequences(conn, 2),
+            HashSet::from_iter(
+                [
+                    "ATCGATCGATAGAGATCGATCGGGAACACACAGAGA",
+                    "ATCATCGATAGAGATCGATCGGGAACACACAGAGA",
+                    "ATCGATCGATCGATCGATCGGGAACACACAGAGA",
+                    "ATCATCGATCGATCGATCGGGAACACACAGAGA"
+                ]
+                .iter()
+                .map(|v| v.to_string())
+            )
+        );
+    }
+
+    #[test]
+    fn test_import_gfa() {
+        let mut gfa_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        gfa_path.push("fixtures/simple.gfa");
+
+        let collection_name = "test".to_string();
+        let conn = &mut get_connection();
+        import_gfa(
+            &gfa_path.to_str().unwrap().to_string(),
+            &collection_name,
+            conn,
+        );
+
+        let result = Path::sequence(conn, &collection_name, None, "124", 1);
+        assert_eq!(result, "ATGGCATATTCGCAGCT");
     }
 }
