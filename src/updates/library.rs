3ab77296baef8398b4d8a8c6286139b31f6603ed
hofer <hofer@ginkgobioworks.com>
2024-10-10 11:45:39 -0400

    Get coordinate math right, fix test

--- src/updates/library.rs
+++ src/updates/library.rs
@@ -45,6 +45,7 @@ pub fn update_with_library(
     let mut parts_reader = fasta::io::reader::Builder.build_from_path(parts_file_path)?;
 
     let mut node_ids_by_name = HashMap::new();
+    let mut sequence_lengths_by_node_id = HashMap::new();
     for result in parts_reader.records() {
         let record = result?;
         let sequence = str::from_utf8(record.sequence().as_ref())
@@ -57,6 +58,7 @@ pub fn update_with_library(
             .save(conn);
         let node_id = Node::create(conn, &seq.hash);
         node_ids_by_name.insert(name, node_id);
+        sequence_lengths_by_node_id.insert(node_id, seq.length);
     }
 
     let library_file = File::open(library_file_path)?;
@@ -68,10 +70,7 @@ pub fn update_with_library(
         .has_headers(false)
         .from_reader(library_reader);
     for result in library_csv_reader.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here..
         let record = result?;
-        println!("{:?}", record);
         let part1_name = record.get(0).unwrap();
         let part2_name = record.get(1).unwrap();
         let part1_id = node_ids_by_name.get(part1_name).unwrap();
@@ -94,16 +93,16 @@ pub fn update_with_library(
         .collect();
     assert_eq!(start_blocks.len(), 1);
     let start_block = start_blocks[0];
-    // TODO: Get this right
-    let node_start_coordinate = start_coordinate - start_block.path_start;
+    let node_start_coordinate =
+        start_coordinate - start_block.path_start + start_block.sequence_start;
+
     let end_blocks: Vec<_> = path_intervaltree
         .query_point(end_coordinate)
         .map(|x| &x.value)
         .collect();
     assert_eq!(end_blocks.len(), 1);
     let end_block = end_blocks[0];
-    // TODO: Get this right
-    let node_end_coordinate = end_coordinate - end_block.path_start;
+    let node_end_coordinate = end_coordinate - end_block.path_start + end_block.sequence_start;
 
     let mut new_edges = HashSet::new();
     for part1 in &parts1 {
@@ -121,10 +120,10 @@ pub fn update_with_library(
     }
 
     for part2 in &parts2 {
+        let part2_source_coordinate = sequence_lengths_by_node_id.get(part2).unwrap();
         let edge = EdgeData {
             source_node_id: **part2,
-            // TODO: Fix this
-            source_coordinate: 0,
+            source_coordinate: *part2_source_coordinate,
             source_strand: Strand::Forward,
             target_node_id: end_block.node_id,
             target_coordinate: node_end_coordinate,
@@ -137,10 +136,10 @@ pub fn update_with_library(
 
     for part1 in parts1 {
         for part2 in &parts2 {
+            let part1_source_coordinate = sequence_lengths_by_node_id.get(part1).unwrap();
             let edge = EdgeData {
                 source_node_id: *part1,
-                // TODO: Fix this
-                source_coordinate: 0,
+                source_coordinate: *part1_source_coordinate,
                 source_strand: Strand::Forward,
                 target_node_id: **part2,
                 target_coordinate: 0,
@@ -155,6 +154,7 @@ pub fn update_with_library(
     let new_edge_ids = Edge::bulk_create(conn, new_edges.iter().cloned().collect());
     BlockGroupEdge::bulk_create(conn, path.block_group_id, &new_edge_ids);
 
+    // TODO: Get this working
     //    OperationSummary::create(operation_conn, operation.id, &summary_str);
     println!("Updated with library file: {}", library_file_path);
     let mut output = Vec::new();
@@ -167,135 +167,63 @@ pub fn update_with_library(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::models::{
-        block_group::{BlockGroup, PathChange},
-        node::Node,
-        path::PathBlock,
-        sequence::Sequence,
-    };
-    use crate::test_helpers::{get_connection, setup_block_group};
+    use crate::imports::fasta::import_fasta;
+    use crate::models::{block_group::BlockGroup, collection::Collection, operations::setup_db};
+    use crate::test_helpers::{get_connection, get_operation_connection, setup_gen_dir};
+    use std::path::PathBuf;
 
-    // TODO: Get this right
-    //    #[test]
+    #[test]
     fn makes_a_pool() {
-        let conn = get_connection(None);
-        let (block_group_id, path) = setup_block_group(&conn);
-        let promoter1 = Sequence::new()
-            .sequence_type("DNA")
-            .sequence("pro1")
-            .save(&conn);
-        let promoter1_node_id = Node::create(&conn, promoter1.hash.as_str());
-        let promoter2 = Sequence::new()
-            .sequence_type("DNA")
-            .sequence("pro2")
-            .save(&conn);
-        let promoter2_node_id = Node::create(&conn, promoter2.hash.as_str());
-        let gene1 = Sequence::new()
-            .sequence_type("DNA")
-            .sequence("gen1")
-            .save(&conn);
-        let gene1_node_id = Node::create(&conn, gene1.hash.as_str());
-        let gene2 = Sequence::new()
-            .sequence_type("DNA")
-            .sequence("gen2")
-            .save(&conn);
-        let gene2_node_id = Node::create(&conn, gene2.hash.as_str());
-        let promoter1_block = PathBlock {
-            id: 0,
-            node_id: promoter1_node_id,
-            block_sequence: promoter1.get_sequence(0, 4).to_string(),
-            sequence_start: 0,
-            sequence_end: 4,
-            path_start: 7,
-            path_end: 15,
-            strand: Strand::Forward,
-        };
-        let promoter2_block = PathBlock {
-            id: 0,
-            node_id: promoter2_node_id,
-            block_sequence: promoter2.get_sequence(0, 4).to_string(),
-            sequence_start: 0,
-            sequence_end: 4,
-            path_start: 7,
-            path_end: 15,
-            strand: Strand::Forward,
-        };
-        let gene1_block = PathBlock {
-            id: 0,
-            node_id: gene1_node_id,
-            block_sequence: gene1.get_sequence(0, 4).to_string(),
-            sequence_start: 0,
-            sequence_end: 4,
-            path_start: 15,
-            path_end: 20,
-            strand: Strand::Forward,
-        };
-        let gene2_block = PathBlock {
-            id: 0,
-            node_id: gene2_node_id,
-            block_sequence: gene2.get_sequence(0, 4).to_string(),
-            sequence_start: 0,
-            sequence_end: 4,
-            path_start: 15,
-            path_end: 20,
-            strand: Strand::Forward,
-        };
-        let changes = vec![
-            PathChange {
-                block_group_id,
-                path: path.clone(),
-                start: 7,
-                end: 15,
-                block: promoter1_block,
-                chromosome_index: 1,
-                phased: 0,
-            },
-            PathChange {
-                block_group_id,
-                path: path.clone(),
-                start: 7,
-                end: 15,
-                block: promoter2_block,
-                chromosome_index: 1,
-                phased: 0,
-            },
-            PathChange {
-                block_group_id,
-                path: path.clone(),
-                start: 15,
-                end: 20,
-                block: gene1_block,
-                chromosome_index: 1,
-                phased: 0,
-            },
-            PathChange {
-                block_group_id,
-                path: path.clone(),
-                start: 15,
-                end: 20,
-                block: gene2_block,
-                chromosome_index: 1,
-                phased: 0,
-            },
-        ];
-        let tree = Path::intervaltree_for(&conn, &path);
-        for change in changes.iter() {
-            BlockGroup::insert_change(&conn, change, &tree);
-        }
+        setup_gen_dir();
+        let mut fasta_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        fasta_path.push("fixtures/simple.fa");
+        let conn = &get_connection(None);
+        let db_uuid = metadata::get_db_uuid(conn);
+        let op_conn = &get_operation_connection(None);
+        setup_db(op_conn, &db_uuid);
+        let collection = "test".to_string();
 
-        let all_sequences = BlockGroup::get_all_sequences(&conn, block_group_id);
+        import_fasta(
+            &fasta_path.to_str().unwrap().to_string(),
+            &collection,
+            false,
+            conn,
+            op_conn,
+        );
+
+        let mut parts_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        parts_path.push("fixtures/parts.fa");
+        let mut library_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        library_path.push("fixtures/combinatorial_design.csv");
+
+        let _ = update_with_library(
+            conn,
+            op_conn,
+            "test",
+            "m123",
+            7,
+            20,
+            parts_path.to_str().unwrap(),
+            library_path.to_str().unwrap(),
+        );
+
+        let block_groups = Collection::get_block_groups(conn, "test");
+        let block_group = &block_groups[0];
+
+        let all_sequences = BlockGroup::get_all_sequences(conn, block_group.id);
         assert_eq!(
             all_sequences,
             HashSet::from_iter(vec![
-                "AAAAAAAAAATTTTTTTTTTCCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAApro1TTTTTCCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAAAAATTTTTgen1CCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAApro1gen1CCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAApro2TTTTTCCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAAAAATTTTTgen2CCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAApro1gen2CCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAApro2gen1CCCCCCCCCCGGGGGGGGGG".to_string(),
-                "AAAAAAApro2gen2CCCCCCCCCCGGGGGGGGGG".to_string(),
+                "ATCGATCGATCGATCGATCGGGAACACACAGAGA".to_string(),
+                "ATCGATCAAAAATGATAAGGAACACACAGAGA".to_string(),
+                "ATCGATCAAAAATGTTAAGGAACACACAGAGA".to_string(),
+                "ATCGATCAAAAATGCTAAGGAACACACAGAGA".to_string(),
+                "ATCGATCTAATATGATAAGGAACACACAGAGA".to_string(),
+                "ATCGATCTAATATGTTAAGGAACACACAGAGA".to_string(),
+                "ATCGATCTAATATGCTAAGGAACACACAGAGA".to_string(),
+                "ATCGATCCAACATGATAAGGAACACACAGAGA".to_string(),
+                "ATCGATCCAACATGTTAAGGAACACACAGAGA".to_string(),
+                "ATCGATCCAACATGCTAAGGAACACACAGAGA".to_string(),
             ])
         );
     }
