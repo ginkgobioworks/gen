16e9a651fbe773fe4d54606586dfca5d77238429
bobvh <1587584+bobvh@users.noreply.github.com>
2025-01-24 16:01:20 -0500

    removed build from precommit hook

--- paper/build.sh
+++ paper/build.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+echo "Building the pdf version of the Gen paper..."
 
 # Check if pandoc is available
 if ! command -v pandoc >/dev/null 2>&1; then
     echo "Error: pandoc is not installed or not in PATH"
     echo "Please install pandoc: https://pandoc.org/installing.html"
-    echo "Continuing without building the pdf version of the Gen paper..."
     exit 0 # Don't abort the commit
 fi
 
 pandoc --citeproc --bibliography=assets/zotero.bib --csl=assets/chicago-author-date.csl \
     --variable documentclass=paper --variable classoption=twocolumn \
     --lua-filter=assets/pandoc_filter.lua --output main.pdf main.md
+
+git add main.pdf
+
+echo "Done!"
